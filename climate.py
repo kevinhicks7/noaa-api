e45f3279e85b90adf41014c9cf4276b69529856c
Kevin Hicks <kevinh1534@gmail.com>
2025-05-11 15:18:44 -0700

    ready to start on evaluating data by city

--- climate.py
+++ climate.py
@@ -6,7 +6,7 @@ from atproto import Client, Request, models
 
 def query_api(cat,params):
     #docs: https://www.ncdc.noaa.gov/cdo-web/webservices/v2
-    ncdc_api_key = os.environ.get('NOAA_API')
+    ncdc_api_key = os.environ.get('NOAA_API') # use reqeusted noaa api token
     if ncdc_api_key is None:
         print('no api key found')
         exit()
@@ -14,7 +14,7 @@ def query_api(cat,params):
     base_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/'
 
     filters = "&".join(f"{param}={val}" for param, val in params.items())
-    query_url = base_url + cat + '?'+ filters
+    query_url = base_url + cat + '?' + filters
     response = requests.get(query_url, headers=headers)
 
     if response == {}:
@@ -26,18 +26,27 @@ def get_cities():
     cities = []
     offset = 0
     while True:
-        loc_params = {'datasetid':'GHCND','locationcategoryid':'CITY','limit':'1000','offset':str(offset)}
+        loc_params = {'datasetid':'GHCND',
+                      'locationcategoryid':'CITY',
+                      'limit':'1000',
+                      'offset':str(offset)}
         loc_batch = query_api(cat='locations',params=loc_params)
-        if 'results'  in loc_batch.keys():
+        if 'results' not in loc_batch.keys() or len(loc_batch['results']) == 0:
+            break
+        else:
             cities += loc_batch['results']
             offset += 1000
-        else:
-            break
     return cities
+
+def eval_city_data(city,data_params):
+    data_params['locationid':city['id']]
+    data = query_api('data',data_params)
+    #evaluate data
+    #post if there's something noteworthy
 
 def post(quote):
     client = Client(request=request)
-    pw = os.environ.get('BSKY_CLIMATE_BOT_PW')
+    pw = os.environ.get('BSKY_CLIMATE_BOT_PW') # from generated app password in bsky
     if pw is None:
         print('no bsky account password found')
         exit()
@@ -50,14 +59,12 @@ def main() -> None:
     #today = datetime.now()
     cities = get_cities()
     us_cities = [city for city in cities if city['id'].startswith('CITY:US')]
-    print(len(us_cities))
-    exit()
 
     data_params = {'datasetid':'GHCND',
-                   'locationid':'ZIP:28801',
                    'startdate':'2010-05-01',
                    'enddate':'2010-05-01'}
-    data = query_api('data',data_params)
+    for city in us_cities:
+        eval_city_data(city)
 
 if __name__ == '__main__':
     main()
