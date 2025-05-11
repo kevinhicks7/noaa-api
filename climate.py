8303cf2535490864d2610e67488b1a631494181e
Kevin Hicks <kevinh1534@gmail.com>
2025-05-11 15:08:43 -0700

    getting all u.s. cities in the database

--- climate.py
+++ climate.py
@@ -1,35 +1,64 @@
 import os
 import requests
 import datetime
+import json
 from atproto import Client, Request, models
 
 def post(quote):
     client = Client(request=request)
-    
     pw = os.environ.get('BSKY_CLIMATE_BOT_PW')
+    if pw is None:
+        print('no bsky account password found')
+        exit()
     client.login('climate-bot.bsky.social', pw)
-    client.send_post(text=quote)
 
-def get_data(today):
+    text = ''
+    client.send_post(text=text)
+
+def query_api(cat,params):
+    #docs: https://www.ncdc.noaa.gov/cdo-web/webservices/v2
     ncdc_api_key = os.environ.get('NOAA_API')
     if ncdc_api_key is None:
-        print('no api key found in environment variables')
+        print('no api key found')
         exit()
-    base_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/data?'
-    data_params = {'datasetid':'GHCND',
-                'locationid':'ZIP:28801',
-                'startdate':'2025-05-08',
-                'enddate':'2025-05-08'}
-    filters = "&".join(f"{param}={val}" for param, val in data_params.items())
-    url = base_url + filters
-
     headers = {'token': ncdc_api_key}
-    req = requests.get(url, headers=headers)
-    return req.json()
+    base_url = 'https://www.ncei.noaa.gov/cdo-web/api/v2/'
+
+    filters = "&".join(f"{param}={val}" for param, val in params.items())
+    query_url = base_url + cat + '?'+ filters
+    response = requests.get(query_url, headers=headers)
+
+    if response == {}:
+        return {}
+    else:
+        return dict(response.json())
+
+def get_cities():
+    cities = []
+    offset = 0
+    while True:
+        loc_params = {'datasetid':'GHCND','locationcategoryid':'CITY','limit':'1000','offset':str(offset)}
+        loc_batch = query_api(cat='locations',params=loc_params)
+        if 'results'  in loc_batch.keys():
+            cities += loc_batch['results']
+            offset += 1000
+        else:
+            break
+    return cities
+
 
 def main() -> None:
-    today = datetime.now()
-    data = get_data(today)
+    #today = datetime.now()
+    cities = get_cities()
+    us_cities = [city for city in cities if city['id'].startswith('CITY:US')]
+    print(len(us_cities))
+    exit()
+
+    data_params = {'datasetid':'GHCND',
+                   'locationid':'ZIP:28801',
+                   'startdate':'2010-05-01',
+                   'enddate':'2010-05-01'}
+    data = query_api('data',data_params)
 
 
 if __name__ == '__main__':
